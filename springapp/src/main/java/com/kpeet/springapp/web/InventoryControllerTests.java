dc847d5e6677311c937388dc000128d988ca94b9
Kenneth Peet D. <kpeet@inf.utfsm.cl>
2016-01-02 15:46:44 -0300

    cap 4 v3 parte 2
    Aspectos incluidos:
    
    4.1. Añadir una referencia a la lógica de negocio en el controlador
    (InventoryController.java - InventoryControllerTests.java)
    
    4.2. Modificar la vista para mostrar datos de negocio y añadir soporte
    para archivos de mensajes (hello.jsp)
    
    4.3. Añadir datos de prueba para rellenar algunos objetos de negocio
    (app-config.xml)
    
    4.4. Añadir una ubicación para los mensajes (messages.properties)

--- springapp/src/main/java/com/kpeet/springapp/web/InventoryControllerTests.java
+++ springapp/src/main/java/com/kpeet/springapp/web/InventoryControllerTests.java
@@ -2,18 +2,25 @@ package com.kpeet.springapp.web;
 
 import static org.junit.Assert.*;
 
+import java.util.Map;
+
 import org.junit.Test;
 import org.springframework.web.servlet.ModelAndView;
 
-public class HelloControllerTests {
+import com.kpeet.springapp.service.SimpleProductManager;
+
+public class InventoryControllerTests {
 
 	 @Test
 	    public void testHandleRequestView() throws Exception{		
-	        HelloController controller = new HelloController();
+	        InventoryController controller = new InventoryController();
+	        controller.setProductManager(new SimpleProductManager());
 	        ModelAndView modelAndView = controller.handleRequest(null, null);		
 	        assertEquals("hello", modelAndView.getViewName());
 	        assertNotNull(modelAndView.getModel());
-	        String nowValue = (String) modelAndView.getModel().get("now");
+		@SuppressWarnings("unchecked")
+	        Map<String, Object> modelMap = (Map<String, Object>) modelAndView.getModel().get("model");
+	        String nowValue = (String) modelMap.get("now");
 	        assertNotNull(nowValue);
 	    }
 
