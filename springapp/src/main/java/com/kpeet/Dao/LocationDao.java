a2372011a1098ec05a9309a067bf9a7962d176c0
kpeet <kpeet@inf.utfsm.cl>
2016-01-07 11:50:33 -0300

    Implementacion de la interface LocationDao, por medio de la clase LocationDaoImpl, la que se encarga de guardar y listar las locasiones almacenadas

--- springapp/src/main/java/com/kpeet/Dao/LocationDao.java
+++ springapp/src/main/java/com/kpeet/Dao/LocationDao.java
@@ -1,6 +1,13 @@
 package com.kpeet.Dao;
 
+import java.util.List;
+
+import com.kpeet.springapp.domain.Location;
+
 public interface LocationDao {
+	public List<Location> getProductList();
+
+    public void saveProduct(Location prod);
 
 
 }
--- springapp/src/main/java/com/kpeet/Dao/LocationDaoImpl.java
+++ springapp/src/main/java/com/kpeet/Dao/LocationDaoImpl.java
@@ -1,5 +1,38 @@
 package com.kpeet.Dao;
 
-public class LocationDaoImpl {
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.kpeet.springapp.domain.Location;
+
+@Repository(value = "locationDao")
+public class LocationDaoImpl implements LocationDao{
+	
+    private EntityManager em = null;
+
+    /*
+     * Sets the entity manager.
+     */
+    @PersistenceContext
+    public void setEntityManager(EntityManager em) {
+        this.em = em;
+    }
+
+    @Transactional(readOnly = true)
+    @SuppressWarnings("unchecked")
+    public List<Location> getProductList() {
+        return em.createQuery("select p from Product p order by p.id").getResultList();
+    }
+
+    @Transactional(readOnly = false)
+    public void saveProduct(Location loc) {
+        em.merge(loc);
+    }
+
 
 }
